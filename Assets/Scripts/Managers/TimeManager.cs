21630d79117dcdf819e16dafe5c6ac4b7c567f95
KwangzSan <tossaporn.santad@gmail.com>
2015-07-30 22:43:01 +0700

    Add time manager

---
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+	public int secondsToEnd;
+	
+	Text text;
+	
+	void Awake ()
+	{
+		text = GetComponent <Text> ();
+	}
+	
+	
+	void Update ()
+	{
+		int timeRemain = secondsToEnd - (int)Time.time;
+		text.text = timeRemain.ToString();
+	}
+}
