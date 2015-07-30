efb580c57aa130545215621dea10eb9ff86420fd
KwangzSan <tossaporn.santad@gmail.com>
2015-07-30 20:08:55 +0700

    Add animation controller

---
+++ Assets/Scripts/MonkeyMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private bool isGround;
+	private Animator animatorAC;
+
+	void Start () {
+		animatorAC = GetComponent <Animator> ();
+	}
+
+	void Update () {
+		if (!isGround) {
+			Vector3 newPosition = transform.position;
+			newPosition.y -= moveSpeed;
+			transform.position = Vector3.Lerp(transform.position, newPosition, moveSpeed * Time.deltaTime);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		isGround = true;
+		animatorAC.SetBool ("IsGround",true);
+	}
+}
